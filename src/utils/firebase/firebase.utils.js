c02055213cdadf0ee143cc8d42f27f5f545ecfd5
Marco Castanho <marco.castanho@toptal.com>
2022-04-27 21:03:17 +0100

    create user documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdgtS1HYzoP7jcplx3-RBH9W51stC_YEM",
@@ -12,14 +13,47 @@ const firebaseConfig = {
   measurementId: "G-4BEM8EF8PQ",
 };
 
-// Initialize Firebase
+/**
+ * Initialize Firebase
+ */
 const firebaseApp = initializeApp(firebaseConfig);
 
+/**
+ * Auth
+ */
 export const auth = getAuth();
 
-// configure google auth
+/**
+ * Firestore DB
+ */
+export const db = getFirestore();
+
+/**
+ * Configure google auth
+ */
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+/**
+ * User Documents
+ */
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.error("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
