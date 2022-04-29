a08ec5995f30feb472be1047071f1fe62785440e
Marco Castanho <marco.castanho@toptal.com>
2022-04-29 12:14:58 +0100

    group recipes routes

--- src/routes/recipes/recipes.component.tsx
+++ src/routes/recipes/recipes.component.tsx
@@ -2,10 +2,10 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { fetchRecipesStart } from "../../store/recipes/recipe.action";
-import { AllRecipes } from "../all-recipes/all-recipes.component";
-import { MyRecipes } from "../my-recipes/my-recipes.component";
-import { IngredientRecipes } from "../ingredient-recipes/ingredient-recipes.component";
-import { TagRecipes } from "../tag-recipes/tag-recipes.component";
+import { AllRecipes } from "./all-recipes.component";
+import { MyRecipes } from "./my-recipes.component";
+import { IngredientRecipes } from "./ingredient-recipes.component";
+import { TagRecipes } from "./tag-recipes.component";
 
 export const Recipes = () => {
   const dispatch = useDispatch();
